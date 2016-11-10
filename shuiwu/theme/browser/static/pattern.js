3f89bdec277af7cd1f23cef4f14736b0a036ce70
adam139 <yuejun.tang@gmail.com>
2016-11-10 20:39:07 +0800

    add table sorter pattern

---
+++ shuiwu/theme/browser/static/pattern.js
@@ -0,0 +1,22 @@
+require([
+  'jquery',
+  'mockup-patterns-base',
+  'tablesorter'
+], function($, Base) {
+  'use strict';
+
+  /* combining bundle and pattern in same file this example */
+
+  Base.extend({
+    name: 'tablesorter',
+    trigger: '.pat-tablesorter',
+    parser: 'mockup',
+    defaults: {
+    },
+    init: function() {
+      var that = this;
+      that.$el.tablesorter();
+    }
+  });
+
+});
